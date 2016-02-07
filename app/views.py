1c6f5d2bd6a10321a2f916287f82346e5a980a87
luigimascolo <lui.mascolo@gmail.com>
2016-02-07 09:59:46 +0100

    - added app/forms.py and app/templates/login.html - modified views.py: login form implemented

--- app/views.py
+++ app/views.py
@@ -1,13 +1,22 @@
 '''
 The views are the handlers that respond to requests from web browsers or other clients. In Flask handlers are written as Python functions. Each view function is mapped to one or more request URLs.
 '''
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from .forms import LoginForm
 
-@app.route('/')
-@app.route('/index')
-def index():
-    user = {'nickname': 'Miguel'} # fake use# fake userr
-    return render_template('index.html',
-                           title='Home',
-                           user=user)
+#@app.route('/')
+#@app.route('/index')
+#def index():
+#    user = {'nickname': 'Miguel'} # fake use# fake userr
+#    return render_template('index.html',
+#                           title='Home',
+#                           user=user)
+
+@app.route('/login', methods=['GET', 'POST']) # without specifying the methods, the view would accept GET requests only
+def login():
+    # Instantiate a form object
+    form = LoginForm()
+    return render_template('login.html',
+                           title='Sign In',
+                           form=form)
