2882e2ce1a9393996d1ce727b75694c16ea2a936
luigimascolo <lui.mascolo@gmail.com>
2016-02-07 10:58:35 +0100

    - receiving and handling form data by using:   - form.validate_on_submit()   - flash   - redirect

--- app/views.py
+++ app/views.py
@@ -5,18 +5,27 @@ from flask import render_template, flash, redirect
 from app import app
 from .forms import LoginForm
 
-#@app.route('/')
-#@app.route('/index')
-#def index():
-#    user = {'nickname': 'Miguel'} # fake use# fake userr
-#    return render_template('index.html',
-#                           title='Home',
-#                           user=user)
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'nickname': 'Miguel'} # fake use# fake userr
+    return render_template('index.html',
+                           title='Home',
+                           user=user)
 
-@app.route('/login', methods=['GET', 'POST']) # without specifying the methods, the view would accept GET requests only
+# without specifying the methods, the view would accept GET requests only
+@app.route('/login', methods=['GET', 'POST']) 
 def login():
     # Instantiate a form object
     form = LoginForm()
+    # Handling submitted data: validate and store the form data
+    if form.validate_on_submit():
+        # The flash function provides feedback to the user.
+        # Show a message on the next page containing the submitted data
+        flash('Login requested for OpenID="%s", remember_me=%s' %
+                (form.openid.data, str(form.remember_me.data)))
+        return redirect('/index')
+    # If data are not validated or not inserted, return the login page
     return render_template('login.html',
                            title='Sign In',
                            form=form)
