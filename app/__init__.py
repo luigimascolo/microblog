cfaeb6fca396c337bae665fdce3c7ab74f1f6a6e
luigimascolo <lui.mascolo@gmail.com>
2016-02-08 07:35:48 +0100

    - added db management scripts

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('config')
-from app import views # do not confuse app variable with app module
+db = SQLAlchemy(app)
+
+from app import views, models # do not confuse app variable with app module
 
--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import os
+
 SRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 
@@ -7,3 +9,13 @@ OPENID_PROVIDERS = [
         {'name': 'AOL',      'url': 'http://openid.aol.com/<username>'},
         {'name': 'Flickr',   'url': 'http://www.flickr.com/<username>'},
         {'name': 'MyOpenId', 'url': 'https://www.myopenid.com'}]
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# SQLALCHEMY_DATABASE_URI is required by the Flask-SQLAlchemy extension.
+# It is the path for the database file.
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+
+# The SQLALCHEMY_MIGRATE_REPO is the folder where to store the SQLAlchemy-migrate
+# data files.
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
